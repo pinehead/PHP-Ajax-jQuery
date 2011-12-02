a75cd89d8a224ad06579854a5fcb4de1e8b94f68
Anthony James <anthony@adjames.com>
2011-12-01 23:16:53 -0600

    Update source for final tut
    This is the final version with updated information for the screencast

--- index.php
+++ index.php
@@ -2,53 +2,49 @@
 <head>
 <title> 
 </title>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" />
-<script type="text/javascript" src="ajax.js" />
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script type="text/javascript">
 
-$(document).ready(function() {
- 
-  $('#submit').click(function() {
- 
- 
-	   var name = $('#name').val();
-	   var email = $('#email').val();
-	   var data = 'name=' + name + '&email=' + email;
-	   
-
-    	$.ajax({
-			type: "POST",
-			url: "php.php",
-			data: data,
-			success: function() {
-			alert("notification sent");
-      	}
-      
-     });
+	$(document).ready(function() {
 	
+		
+		$('#submit').click(function() {
+		
+		
+		
+			var name = $('#name').val();
+			var email = $('#email').val();
+			
+			var varData = 'name=' + name + '&email=' + email;
+			console.log(varData);
+				
+			$.ajax({
+				type: "POST",
+				url:'php.php',
+				data: varData,
+				success: function() {
+					alert("It was a success");
+				}
+			
+			});
+		
+		});
+	
+	
+	});
 
-     });    
 
-});
 </script>
-
 </head>
 <body>
 
-<div id="#notification">
- 
-</div>
-
-<input type="text" id="name" />
+<input type="text" name="name" id="name" />
 <label for="name">Name</label>
 <br />
-<input type="text" id="email" />
+<input type="text" name="email" id="email" />
 <label for="email">Email</label>
-<div id="submit" style="background-color:#cccccc;">
-Send me an email
-</div>
 
-
+<div id="submit" style="background-color:#cccccc;">Click me to submit</div>
 
 </body>
 </html>
